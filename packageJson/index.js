89fb7d32ab365c6bc43942859fbb221657c79544
Aniket Pathak <aniketpathak1370@gmail.com>
2024-06-24 12:59:24 +0530

    add new

--- packageJson/index.js
+++ packageJson/index.js
@@ -1,1 +1,11 @@
-console.log("package.json")
+// const colors = require('colors');
+
+// console.log("package.json".green)
+
+var colors = require('colors/safe');
+ 
+console.log(colors.green('hello')); // outputs green text
+console.log(colors.red.underline('i like cake and pies')) // outputs red underlined text
+console.log(colors.inverse('inverse the color')); // inverses the color
+console.log(colors.rainbow('OMG Rainbows!')); // rainbow
+console.log(colors.trap('Run the trap')); // Drops the bass
