817ff65ca41545d13419c71a84c34ee014e1af9c
Aniket Pathak <aniketpathak1370@gmail.com>
2024-06-27 21:13:11 +0530

    add router

--- Make_Basic_server/index.js
+++ Make_Basic_server/index.js
@@ -1,8 +1,35 @@
 
 const http = require('http')
 
-http.createServer((req , resp)=>{
-    resp.write("Hello, This is first basic server");
-    resp.end()
+// http.createServer((req , resp)=>{
+//     resp.write("Hello, This is first basic server");
+//     resp.end()
 
-}).listen(4500)
+// }).listen(4500)
+
+//Another method to creat serever
+
+//Routing
+
+//Step 1: create server
+const fs = require('fs')
+const html = fs.readFileSync('./index.html','utf-8')
+const server = http.createServer((request, response)=>{
+
+    let path = request.url
+
+    if(path === '/' || path.toLocaleLowerCase() === '/home'){
+        response.end(html.replace('{{%CONTENT%}}', 'You are in Home page'));
+    }else if(path.toLocaleLowerCase() === '/about'){
+        response.end(html.replace('{{%CONTENT%}}', "You are in About page"));
+    }else if(path.toLocaleLowerCase() === '/contact'){
+        response.end(html.replace('{{%CONTENT%}}', "You are in Contact page"));
+    }else{
+        response.end(html.replace('{{%CONTENT%}}',"Error 404: Page not found"));
+    }
+
+});
+//Step 2: Start server
+server.listen(8000,'127.0.0.1', ()=>{
+    console.log('server has started')
+})
